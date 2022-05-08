822b4b6858d37d6dd3e562c34def3334e3cd5036
manolobkno08 <manologm10@hotmail.com>
2022-05-08 15:55:48 -0500

    ADD: Validation into main loop

--- main.py
+++ main.py
@@ -8,44 +8,68 @@ import sys
 import os
 
 
+def read_file():
+    """Method that allows to read a specific file."""
+    choose_file = input("""
+                        ==== Read File - ACME ====
+                        Please, enter the name of the file:
+
+                        Usage:      filename.txt\n
+                        Enter filename: """)
+    try:
+        with open(choose_file, "r", encoding="utf-8") as f:
+            print(f.read(), end="")
+    except Exception:
+        print(f"\n\t=>  File '{choose_file}' not found")
+
+
 def ACMECommand():
     """
-    Start program
+    Start program menu
 
     """
+    c = 0
     run = True
+    username = input("\t=>  Enter your username: ").upper()
 
     while run:
-        try:
-            choise = int(input("""
-            ==== Welcome to ACME ====
-            Please choose your option:
+        if len(username) > 0:
+            if username.isspace() == False:
+                try:
+                    choise = int(input(f"""
+                        ==== ¡{username}! Welcome to ACME ====
+                        Please, choose one of the following options:
 
-            1      Read file
-            2      Exit
+                        1      Read file
+                        2      Exit
 
-            Your option: """))
+                        Your option: """))
 
-            if choise < 0 or choise != 1 or choise != 2:
+                    if choise == 1:
+                        os.system("clear")
+                        read_file()
+
+                    elif choise == 2:
+                        run = False
+                        print(f"\n\t=>  See you later {username}!\n")
+
+                    elif choise < 0 or choise != 1 or choise != 2:
+                        os.system("clear")
+                        print("\t=>  Option not valid")
+
+                except ValueError:
+                    os.system("clear")
+                    print("\t=>  The option must be a number")
+            else:
                 os.system("clear")
-                print("\t=>  Option not valid")
-
-            if choise == 1:
-                os.system("clear")
-                print("\t=>  SELECT FILE")
-
-            if choise == 2:
-                run = False
-                os.system("clear")
-                print("\t=>  See you soon!\n")
-        except ValueError:
+                username = input(
+                    "\t=>  User not valid, enter your username again: ").upper()
+        else:
             os.system("clear")
-            print("\t=>  The option must be a number")
+            username = input(
+                "\t=>  Please, enter your username again: ").upper()
+        c += 1
 
-
-# Read files from arguments
-# file = sys.argv[1]
-# print(file)
 
 if __name__ == "__main__":
     """
